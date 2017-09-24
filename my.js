82138a32ec52ede36ed987f944772d1154274a80
emmadema <emma.demango@gmail.com>
2017-09-24 15:28:17 -0600

    they move on key click

--- my.js
+++ my.js
@@ -4,21 +4,37 @@ $(document).ready(function() {
 	console.log('ready');
 
 //a keycode
-	$('.luigi').keyup(function(e) {
+	/*$('.luigi').keyup(function(e) {
 		if (e.keyCode == 65)
-		console.log("luigi clicked");
+		console.log('luigi clicked');
 		//$(this).animate({'left': '+=20px'});
 	});
 
 //l keycode
-	$('.mario').keyup(function(e) {
-		if(e.keyCode == 76) 
-			console.log("mario");
+	$('.mario').keyup(function(e){
+		alet('a key was pressed');
+		if(e.keyCode == 76) {
+			console.log('mario');
 			//$(this).animate({'left': '+=20px'});
-		
-	});
+		}
+	});*/
 
+ $(document).keydown(function(e){
 
+ 	var positionL = $('.luigi').position();
+ 	var positionM = $('.mario').position();
+
+ 	switch (e.keyCode)
+ 		{
+ 			case 65: // a
+ 			$('.luigi').css('left', positionL.left + 20 + 'px');
+ 			break;
+
+ 			case 76: //l
+ 			$('.mario').css('left', positionM.left + 20 + 'px');
+ 			break;
+ 		}
+ });
 });
 
 
