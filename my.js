8220734b4ea2bcaab01f409a27d129e9e7a0dcaa
emmadema <emma.demango@gmail.com>
2017-09-24 20:23:35 -0600

    attemp on at win function

--- my.js
+++ my.js
@@ -1,8 +1,9 @@
 
-//load in jQuery
+//loads document jQuery and says ready in the console when j query is uploaded
 $(document).ready(function() {
 	console.log('ready');
 
+//orginal code I tried - did not work but saved for refernce
 //a keycode
 	/*$('.luigi').keyup(function(e) {
 		if (e.keyCode == 65)
@@ -10,7 +11,7 @@ $(document).ready(function() {
 		//$(this).animate({'left': '+=20px'});
 	});
 
-//l keycode
+	//l keycode
 	$('.mario').keyup(function(e){
 		alet('a key was pressed');
 		if(e.keyCode == 76) {
@@ -19,29 +20,51 @@ $(document).ready(function() {
 		}
 	});*/
 
+	//make mario head move on click
+//select mario head
+//set click function to key 'a'
+// set keystroke a to move mario head 10px each click
+
  $(document).keydown(function(e){
 
- 	var positionL = $('.luigi').position();
- 	var positionM = $('.mario').position();
+ 	var positionL = $('.luigi').position();//luigis position
+ 	var positionM = $('.mario').position();//marios position
 
  	switch (e.keyCode)
  		{
  			case 65: // a
  			$('.luigi').css('left', positionL.left + 20 + 'px');
  			break;
-
+ 			//this says the div with the class of lugi will move when a is pressed 20px to the left
+ 			//it acives the movement by adjusting the  Css styling accordingly
  			case 76: //l
  			$('.mario').css('left', positionM.left + 20 + 'px');
  			break;
+ 			//mario works the same way as luigi but it moves with the L key
  		}
  });
+
+//win condition
+//when one div reaches 51 clicks that div wins
+// or when one div crosses anothe div that one wins
+var mario = $('.mario');
+var luigi = $('.luigi');
+
+ function winner() {
+ 	if (mario = '1000px'){
+ 		alert('Mario wins!');
+ 	} else if (luigi = '1000px'){ 
+ 		alert('Luigi wins!');
+ 	}
+
+ }
+
 });
 
+// winner annouced after 51 clicks - how many clicks it takes to get to the end of the page
+// either div is touching the end annouce mario or luigi wins
 
-//make mario head move on click
-//select mario head
-//set click function to key 'a'
-// set keystroke a to move mario head 10px each click
+
 
 
 
