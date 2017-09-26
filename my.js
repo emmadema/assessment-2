8640bc4112b6852e19ca801765f50e3805543cc2
emmadema <emma.demango@gmail.com>
2017-09-25 18:00:42 -0600

    win state

--- my.js
+++ my.js
@@ -3,74 +3,51 @@
 $(document).ready(function() {
 	console.log('ready');
 
-//orginal code I tried - did not work but saved for refernce
-//a keycode
-	/*$('.luigi').keyup(function(e) {
-		if (e.keyCode == 65)
-		console.log('luigi clicked');
-		//$(this).animate({'left': '+=20px'});
-	});
 
-	//l keycode
-	$('.mario').keyup(function(e){
-		alet('a key was pressed');
-		if(e.keyCode == 76) {
-			console.log('mario');
-			//$(this).animate({'left': '+=20px'});
-		}
-	});*/
+	 var marioClicks = 0;//tracking clicks for mario
+	 var luigiClicks = 0; //tracking clicks for luigi
 
-	//make mario head move on click
-//select mario head
-//set click function to key 'a'
-// set keystroke a to move mario head 10px each click
+	 $(document).keydown(function(e){
 
- $(document).keydown(function(e){
+	 	var positionL = $('.luigi').position();//luigis position
+	 	var positionM = $('.mario').position();//marios position
 
- 	var positionL = $('.luigi').position();//luigis position
- 	var positionM = $('.mario').position();//marios position
 
- 	switch (e.keyCode)
- 		{
+	 	switch (e.keyCode) {
  			case 65: // a
- 			$('.luigi').css('left', positionL.left + 20 + 'px');
- 			break;
+	 			$('.luigi').css('left', positionL.left + 20 + 'px');
+	 			luigiClicks = luigiClicks+1;
+	 			break;
  			//this says the div with the class of lugi will move when a is pressed 20px to the left
  			//it acives the movement by adjusting the  Css styling accordingly
  			case 76: //l
- 			$('.mario').css('left', positionM.left + 20 + 'px');
- 			break;
- 			//mario works the same way as luigi but it moves with the L key
- 		}
+	 			$('.mario').css('left', positionM.left + 20 + 'px');
+	 			marioClicks = marioClicks+1;
+	 			break;
+ 				//mario works the same way as luigi but it moves with the L key
+	 	}
+	 	//win state - the person wins after 50 clicks and an annoucment is made
+		function click () {
 
- 	$(function() {
+			if (luigiClicks === 50){
+				alert('Luigi wins!');
+			} else if (marioClicks === 50) {
+				alert('Mario Wins!');
+			}
+		}
 
- 		if (positionM.on() === 500 + 'px') {
- 			alert('Mario wins!');
+		console.log(luigiClicks);
+		console.log(marioClicks);
+	 	click();
 
- 		} else if (positionL.on() === 500 + 'px') { 
- 			alert('Luigi wins!');
+	 });
 
- 		}
-
- 	});
-
- });
-
-
-//var mario = $('.mario');
-//var luigi = $('.luigi');
-
-
-//win condition
-//when one div reaches 51 clicks that div wins
-// or when one div crosses anothe div that one wins
-
+	 
 
 });
 
 
-// winner annouced after 51 clicks - how many clicks it takes to get to the end of the page
+// winner annouced after 50 clicks - how many clicks it takes to get to the end of the page
 // either div is touching the end annouce mario or luigi wins
 
 
